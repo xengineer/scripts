17ffe7f01ef2c14359074accc6eb90a20db81375
hideaki nemoto <hideaki.nemoto@nubee.com>
2013-10-31 10:57:48 +0900

    ゲーム名と、ゲーム会社、両方抜けるようにしてみた。

--- analannie.rb
+++ analannie.rb
@@ -6,14 +6,17 @@ f = File.open("test.txt")
 
 docs = Nokogiri::XML.parse(f)
 #app_free_download = docs.css("tr td[class=\"app free\"] span:not[title=\"This App has In-App Purchases\"] span[class=\"oneline-info title-info\"] a")
-app_free_download = docs.css("tr > td[class=\"app free\"]:first-child span[class=\"oneline-info title-info\"] a")
+#app_free_download = docs.css("tr > td[class=\"app free\"]:first-child div[class=\"main-info\"] span[class=\"oneline-info title-info\"] span[class=\"oneline-info add-info\"] a")
+#app_free_download = docs.css("tr > td[class=\"app free\"]:first-child div[class=\"main-info\"] span a")
+app_free_download = docs.css("tr > td[class=\"app free\"]:first-child div[class=\"main-info\"]")
 #app_free_grossing = docs.css("tr span a")
 companies = docs.css("tr span[data-items]")
 
 app_free_download.each { |game|
-  p game.content
+  gname   = game.css("span[class=\"oneline-info title-info\"] a")
+  company = game.css("span[class=\"oneline-info add-info\"] a")
+  p gname.text()
+  p company.text()
+  print "\n"
 }
 
-#companies.each { |company|
-#  p company.content
-#}
