3edb503ea06e0a4bda9d38285ebac9f9bc6cb789
Hideaki Nemoto <nemoto_hideaki@Hideaki-no-MacBook-Pro.local>
2013-11-06 00:45:21 +0900

    yaml読み込みとか追加した

--- analannie.rb
+++ analannie.rb
@@ -1,22 +1,38 @@
 # - - coding: utf-8
 
 require "nokogiri"
+require "yaml"
 
+unless ARGV[0].nil? then
+end
+if ARGV.size > 0 and ARGV[0] == "--inv"
+  mode = "investigation"
+end
+
+config = YAML.load_file("company.yaml")
 f = File.open("test.txt")
 
 docs = Nokogiri::XML.parse(f)
-#app_free_download = docs.css("tr td[class=\"app free\"] span:not[title=\"This App has In-App Purchases\"] span[class=\"oneline-info title-info\"] a")
-#app_free_download = docs.css("tr > td[class=\"app free\"]:first-child div[class=\"main-info\"] span[class=\"oneline-info title-info\"] span[class=\"oneline-info add-info\"] a")
-#app_free_download = docs.css("tr > td[class=\"app free\"]:first-child div[class=\"main-info\"] span a")
 app_free_download = docs.css("tr > td[class=\"app free\"]:first-child div[class=\"main-info\"]")
-#app_free_grossing = docs.css("tr span a")
-companies = docs.css("tr span[data-items]")
 
-app_free_download.each { |game|
-  gname   = game.css("span[class=\"oneline-info title-info\"] a")
-  company = game.css("span[class=\"oneline-info add-info\"] a")
-  p gname.text()
-  p company.text()
-  print "\n"
-}
+case mode
+when "investigation" then
+  app_free_download.each { |game|
+    gname   = game.css("span[class=\"oneline-info title-info\"] a")
+    company = game.css("span[class=\"oneline-info add-info\"] a")
 
+    if !config.has_key?(company.text())
+      print company.text() + ": \n"
+    end
+  }
+else
+  app_free_download.each { |game|
+    gname   = game.css("span[class=\"oneline-info title-info\"] a")
+    company = game.css("span[class=\"oneline-info add-info\"] a")
+    if config.has_key?(company.text())
+      print gname.text() + " " + company.text() + " " + config[company.text()] + "\n"
+    else
+      print gname.text() + " " + company.text() + " " + "unknown\n"
+    end
+  }
+end
